9d9091ce330d4ab4535e268109e9950118d38d63
xQund <paul.kramer1@hotmail.com>
2025-03-25 14:18:09 +0100

    V2

--- Projekt_Kikeriki.py
+++ Projekt_Kikeriki.py
@@ -1,19 +1,34 @@
+#Projekt_Kikeriki
+#11.03.2025   Paul Kramer
+#Letzte Änderung: 25.03.2025
+#Steuern einer Hühnerklappe und Zählen der Hühner
+#ESP32-S3
+
+#---------------------------------------------------------------------------------------------------------------
 #########Bibliotheken#########
 import time
 from mfrc522 import MFRC522
 from machine import Pin, SoftSPI
 import WLAN
 import umqtt.simple
+import I2C_Bus as sensor
 import ujson
 
+#---------------------------------------------------------------------------------------------------------------
 #########Variablen#########
 zaehler = 0
+lux = 0
 draussen = []
 drinnen = []
+klappe = Pin(8, Pin.OUT)
+hell_zeit = time.ticks_ms()
+dunkel_zeit = time.ticks_ms()
 
+#---------------------------------------------------------------------------------------------------------------
 #########WLAN#########
 WLAN.STA()
 
+#---------------------------------------------------------------------------------------------------------------
 #########Initialisierung vom SPI#########
 spi = SoftSPI(
         baudrate=20000000,
@@ -25,7 +40,34 @@ spi = SoftSPI(
 
 RFID = MFRC522(spi, rst=Pin(45, Pin.OUT), cs=Pin(36, Pin.OUT))
 
+#---------------------------------------------------------------------------------------------------------------
+#########MQTT#########
+mqtt_client = umqtt.simple.MQTTClient("Kikeriki_ESP32", "192.168.1.165", 1883)
+
+#---------------------------------------------------------------------------------------------------------------
+#########JSON#########
+def json_format():
+    return {
+            "Helligkeit":lux,
+            "Draussen": len(draussen),
+            "Drinnen": len(drinnen)
+            }
+
+#---------------------------------------------------------------------------------------------------------------
+#########Hauptprogramm#########
 while True:
+    lux = sensor.lux()
+    if lux >= 300:
+        hell_zeit = time.ticks_ms()
+    elif lux < 100:
+        dunkel_zeit = time.ticks_ms()
+        
+    if time.ticks_diff(hell_zeit, dunkel_zeit) > 5000:
+        klappe.on()
+        
+    elif time.ticks_diff(dunkel_zeit, hell_zeit) > 5000:
+        klappe.off()
+        
     card = RFID.read_card()
     if card == None:
         pass
@@ -43,7 +85,15 @@ while True:
             pass
     else:
         time.sleep(1)
+        
+    json_daten = ujson.dumps(json_format())
     
-    print(draussen)
+    try:
+        mqtt_client.connect()
+        mqtt_client.publish("Kikeriki/ESP32/Daten", json_daten)
+        mqtt_client.disconnect()
+    except:
+        print("Fehler bei MQTT")
+    
     
         
